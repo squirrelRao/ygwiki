b8f979b09fba3d04061b88e65162f0d655c2f904
chaosconst <yuanxy@163.com>
2013-04-18 13:55:38 +0800

    update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,7 +5,7 @@ cat /tmp/a | sed 's|</TD><TD>|</TD><TD>|g'| tr '' '\n' > $1
 cat $1 | sed 's|</div><|</div><|g' | tr '' '\n' > /tmp/a
 cat /tmp/a | sed 's|</DIV><|</DIV><|g'| tr '' '\n' > $1
 
-cat $1 | sed 's|><div>|><div>|g' | tr '' '\n' > /tmp/a
-cat /tmp/a | sed 's|><DIV>|><DIV>|g'| tr '' '\n' > $1
+cat $1 | sed 's|><div|><div|g' | tr '' '\n' > /tmp/a
+cat /tmp/a | sed 's|><DIV|><DIV|g'| tr '' '\n' > $1
 
 
