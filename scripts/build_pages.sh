f6ed07618973fee8bbdeec2613c90fa24a922326
Yuan Xingyuan <yuanxy@163.com>
2013-08-10 13:31:00 +0800

    add build pages script

---
+++ scripts/build_pages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+src/hdwiki2mediawiki.feedback.py data/ygwiki_lesson_feedback
+src/hdwiki2mediawiki.plan.py data/ygwiki_lesson_plan
+src/hdwiki2mediawiki.review.py data/ygwiki_lesson_review
